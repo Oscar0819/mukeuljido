1c4f728eb474e4f78d3816c0c305742f9ca25be0
박은우 <dn0819@kissoft.co.kr>
2023-01-08 14:41:56 +0900

    - init commit

---
+++ app/src/main/java/com/milet0819/mukeuljido/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.milet0819.mukeuljido.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.milet0819.mukeuljido.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
